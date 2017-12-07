8c7f8e8c92a05060031b22eeee2ccfae351de083
majie <majie@windows10.microdone.cn>
2017-12-07 16:35:50 +0800

    修改验证码重发机制

--- src/main/java/com/lichi/increaselimit/common/Constants.java
+++ src/main/java/com/lichi/increaselimit/common/Constants.java
@@ -12,7 +12,11 @@ public interface Constants {
 	 */
 	public static final String MOBILE_REDIS_KEY = "code:sms:";
 	/**
-	 * 验证码存放在redis的时间
+	 * 验证码存放在redis的时间900s
 	 */
-	public static final Long CODE_IN_REDIS_TIME = 60L;
+	public static final Long CODE_IN_REDIS_TIME = 900L;
+	/**
+	 * 发送验证码的间隔时间
+	 */
+	public static final int CODE_RESEND = 60;
 }
--- src/main/java/com/lichi/increaselimit/security/validate/code/sms/SmsCodeGenerator.java
+++ src/main/java/com/lichi/increaselimit/security/validate/code/sms/SmsCodeGenerator.java
@@ -1,10 +1,13 @@
 package com.lichi.increaselimit.security.validate.code.sms;
 
+import java.time.LocalDateTime;
+
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.ServletWebRequest;
 
+import com.lichi.increaselimit.common.Constants;
 import com.lichi.increaselimit.common.enums.ResultEnum;
 import com.lichi.increaselimit.common.exception.BusinessException;
 import com.lichi.increaselimit.security.properties.SecurityProperties;
@@ -28,14 +31,21 @@ public class SmsCodeGenerator implements ValidateCodeGenerator {
 	private RedisValidateCodeRepository<ValidateCode> redisValidateCodeRepository;
 	
 	/**
-	 * 先校验验证码是否存在，如果存在一分钟后再发
+	 * 先校验验证码是否存在
+	 * 如果存在看生成时间,60秒后才允许重发
 	 */
 	@Override
 	public ValidateCode generate(ServletWebRequest request) {
 		
 		ValidateCode validateCode = redisValidateCodeRepository.get(request, ValidateCodeType.SMS);
+		
 		if(validateCode != null) {
-			throw new BusinessException(ResultEnum.CODE_EXIST);
+			LocalDateTime localDateTime = LocalDateTime.now();
+			LocalDateTime minusSeconds = validateCode.getExpireTime().minusSeconds(Constants.CODE_IN_REDIS_TIME).plusSeconds(Constants.CODE_RESEND);
+			
+			if(minusSeconds.compareTo(localDateTime) > 0) {
+				throw new BusinessException(ResultEnum.CODE_EXIST);
+			}
 		}
 		String code = RandomStringUtils.randomNumeric(securityProperties.getCode().getSms().getLength());
 		code="123456";
