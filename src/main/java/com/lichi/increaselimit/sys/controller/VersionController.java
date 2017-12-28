577fb0b61e4fd40d897e8f54bca8f90c60b3c277
majie <majie@windows10.microdone.cn>
2017-12-28 17:13:28 +0800

    添加文件提交

--- src/main/java/com/lichi/increaselimit/sys/controller/VersionController.java
+++ src/main/java/com/lichi/increaselimit/sys/controller/VersionController.java
@@ -13,10 +13,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.github.pagehelper.PageInfo;
+import com.lichi.increaselimit.common.utils.FileUtil;
 import com.lichi.increaselimit.common.utils.ResultVoUtil;
 import com.lichi.increaselimit.common.vo.ResultVo;
 import com.lichi.increaselimit.sys.controller.dto.VersionDto;
@@ -84,5 +87,25 @@ public class VersionController {
 		log.info("查询版本信息,版本id:{}",id);
 		Version list = versionService.selectOne(id);
 		return ResultVoUtil.success(list);
+	}
+	
+	
+	@ApiOperation(value = "文件上传")
+	@RequestMapping(value = "/upload",method=RequestMethod.POST)
+	public ResultVo<Object> upload(@RequestParam("file") MultipartFile file) {
+		if (file.isEmpty()) {
+			return ResultVoUtil.error(1, "文件内容为空");
+		}
+		// 获取文件名
+
+		String fileName = file.getOriginalFilename();
+		log.info("上传的文件名为：" + fileName);
+		
+		try {
+			FileUtil.uploadFile(file.getBytes(), "/home/homepage/release", fileName);
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+		return ResultVoUtil.success();
 	}
 }
