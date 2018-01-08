3907457d0683f32840417b4c8a85e2299e7c63e1
majie <majie@windows10.microdone.cn>
2018-01-08 12:17:41 +0800

    修改资源查询

--- src/main/java/com/lichi/increaselimit/sys/dao/SysRoleResourceDao.java
+++ src/main/java/com/lichi/increaselimit/sys/dao/SysRoleResourceDao.java
@@ -17,13 +17,14 @@ import com.lichi.increaselimit.sys.entity.SysRoleResource;
 @Mapper
 public interface SysRoleResourceDao extends BaseMapper<SysRoleResource> {
 
-	@Select("select e.*,m.menu_name,t.button_name from t_sys_user b,t_sys_role c,t_sys_user_role d,t_sys_role_resource e ,t_sys_menu m ,t_sys_button t " + 
-			"where e.role_id = c.id " + 
-			"and c.id = d.role_id " + 
-			"and d.user_id = b.id " + 
-			"and e.menu_id = m.id " + 
-			"and e.button_id = t.id " + 
-			"and b.id = #{userId}")
+	@Select("select e.*,m.menu_name,t.button_name  " + 
+			"from t_sys_role_resource e  " + 
+			"INNER JOIN t_sys_role c on e.role_id = c.id  " + 
+			"INNER JOIN t_sys_user_role d on c.id = d.role_id  " + 
+			"INNER JOIN t_sys_user b on d.user_id = b.id " + 
+			"INNER JOIN t_sys_menu m on e.menu_id = m.id " + 
+			"LEFT JOIN t_sys_button t on e.button_id = t.id " + 
+			"where b.id = #{userId}")
 	List<SysRoleResource> selectUserResource(String userId);
 
 }
